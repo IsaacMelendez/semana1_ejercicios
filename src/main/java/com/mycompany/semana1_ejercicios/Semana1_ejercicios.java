ba565bc69d8aebe819ce4d98d5166aad9b2d393b
Isaac Meléndez <gmelendezmamani@gmail.com>
2025-05-13 19:48:03 -0500

    crear proyecto

---
+++ src/main/java/com/mycompany/semana1_ejercicios/Semana1_ejercicios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana1_ejercicios;
+
+/**
+ *
+ * @author isaac
+ */
+public class Semana1_ejercicios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
